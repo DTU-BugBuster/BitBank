250798e0a17cee59b674bb7b0456089a39ebbb56
Dhruv Jain <dhruvjain2212000@gmail.com>
2018-10-27 18:53:01 +0530

    hack begins

--- app/src/main/java/com/example/android/bitbank/MainActivity.java
+++ app/src/main/java/com/example/android/bitbank/MainActivity.java
@@ -6,7 +6,6 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.android.bitbank.InitialStatics.IntroActivity;
 
 import static com.example.android.bitbank.GlobalAccess.COMPLETED_ONBOARDING_PREF_NAME;
 
@@ -21,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // Check if we need to display our OnboardingFragment
         if (!sharedPreferences.getBoolean(COMPLETED_ONBOARDING_PREF_NAME, false)) {
             // The user hasn't seen the OnboardingFragment yet, so show it
-            startActivity(new Intent(this, IntroActivity.class));
+            startActivity(new Intent(this, BoardingActivity.class));
         }
     }
 }
